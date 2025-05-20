6bef88e62e9f7d7eb15e19b04264de57481ca622
Rafi Zaman <rafiirfan211@gmail.com>
2025-05-20 18:03:56 +0600

    UI is complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import TopBanner from "./components/TopBanner";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent.jsx";
+import Newsletter from "./components/Newsletter.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <TopBanner />
         <Navbar />
         <MainContent />
+        <Newsletter />
+        <Footer />
       </div>
     </>
   );
